41493a6967fb0859879fdc9bd61ca65b2a384fd8
Joseph Siebert <josephsiebert@ip0af51358.int.uvm.edu>
2016-04-02 10:24:16 -0400

    Changing top

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
+<?php
+include "top.php";
+?>
 
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        
-        <title>Try me page </title>
-    </head>
-    <body>
-        <p>
-        <?php
-       print "Soda is actually good for you, secretly...shh! No"
-        ?>
-        </p>
-    </body>
-</html>
+<body>
+    <p>
+    <?php
+   print "Soda is actually good for you, secretly...shh! No"
+    ?>
+    </p>
+</body>
