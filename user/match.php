a0c3b5bab2be97ab314e1d8535e692dc44ee089b
Joseph Siebert <josephsiebert@ip0af51358.int.uvm.edu>
2016-04-02 11:56:43 -0400

    Move match.php

---
+++ user/match.php
@@ -0,0 +1,34 @@
+
+<h1>Meet Your Matches!</h1>
+
+<?php
+
+
+
+?>
+
+<?php
+    include("top.php");
+?>
+
+<p id="des">Welcome to your habitat, a place for you, where you belong. Our team has carefully selected people we feel will complement your living style. Start emailing your matches to have your perfect college roommate experience here at UVM!</p>
+
+<table>
+    <tbody>
+        <tr>
+            <td>Name</td>
+            <td>Gender</td>
+            <td>Year</td>
+            <td>On/Off Campus?</td>
+            
+        </tr>
+        
+        <tr>
+            <td>Sally Sunshine</td>
+            <td>Female</td>
+            <td>Year 3</td>
+            <td>Off Campus</td>
+            
+        </tr>
+    </tbody>
+</table>
