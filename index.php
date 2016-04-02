35631478f80c9c8939bddc4343882bee8de65fda
sopalmer <sopalmer@uvm.edu>
2016-04-02 09:43:41 -0400

    Test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <title>Try me page </title>
+    </head>
+    <body>
+        <p>
+        <?php
+       print "Soda is actually good for you, secretly...shh!"
+        ?>
+        </p>
+    </body>
+</html>
