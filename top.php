bd7d1cdef5d2364f99ece9d7b213b3c2439e4455
Joseph Siebert <josephsiebert@ip0af51358.int.uvm.edu>
2016-04-02 10:39:41 -0400

    Fixing more typos

--- top.php
+++ top.php
@@ -33,8 +33,8 @@ require_once('lib/custom-functions.php');
         //     www-root
         
         
-        $includeDBPath = "../bin/";
-        $includeLibPath = "../lib/";
+        $includeDBPath = "/bin/";
+        $includeLibPath = "/lib/";
         
         
         // require_once('lib/mailMessage.php');
