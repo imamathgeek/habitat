fcdc25b08a7d6f8b88d19aa547af216ee71dbe28
sopalmer <sopalmer@uvm.edu>
2016-04-02 12:57:57 -0400

    fixed duplications

--- index.php
+++ index.php
@@ -60,6 +60,17 @@ $year = "Freshman";
                    onfocus="this.select()" >
 
         </fieldset>
+
+              <fieldset class="contact">
+                <label for="txtUserName" class="required"> Username
+                    <input type="text" id="txtUserName" name="txtUserName"
+                           value="<?php print $username; ?>"
+                           tabindex="100"  maxlength="45" placeholder="Enter your username"
+                           <?php if ($usernameERROR) print 'class="mistake"'; ?>
+                           onfocus="this.select()"
+                           autofocus>
+                </label>
+                </fieldset>
         
          <!-- gender -->
         <fieldset class="radio">
@@ -90,27 +101,8 @@ $year = "Freshman";
                            tabindex="340"> Prefer not to disclose</label>
         </fieldset>
 
-         <fieldset class="contact">
-            <label for="txtUserName" class="required"> Username
-                <input type="text" id="txtUserName" name="txtUserName"
-                       value="<?php print $username; ?>"
-                       tabindex="100"  maxlength="45" placeholder="Enter your username"
-                       <?php if ($usernameERROR) print 'class="mistake"'; ?>
-                       onfocus="this.select()"
-                       autofocus>
-            </label>
-            </fieldset>
-            
-                <fieldset class="contact">
-                <label for="txtUserName" class="required"> Username
-                    <input type="text" id="txtUserName" name="txtUserName"
-                           value="<?php print $username; ?>"
-                           tabindex="100"  maxlength="45" placeholder="Enter your username"
-                           <?php if ($usernameERROR) print 'class="mistake"'; ?>
-                           onfocus="this.select()"
-                           autofocus>
-                </label>
-                </fieldset>
+         
+ 
   
             <fieldset  class="lists">
                 <label for="lstDesc"> Year </label>
@@ -132,38 +124,6 @@ $year = "Freshman";
                 </select>
             </fieldset>
 
-
-            
-             <!-- gender -->
-            <fieldset class="radio">
-                <p>What is your gender?</p>
-                <label><input type="radio" 
-                              id="radGenderMale" 
-                              name="radGender" 
-                              value="Male"
-                              <?php if ($gender == "Male") print 'checked' ?>
-                              tabindex="330"> Male</label>
-                <label><input type="radio" 
-                              id="radGenderFemale" 
-                              name="radGender" 
-                              value="Female"
-                              <?php if ($gender == "Female") print 'checked' ?>
-                              tabindex="340"> Female</label>
-                <label><input type="radio" 
-                              id="radGenderOther" 
-                              name="radGender" 
-                              value="Other"
-                              <?php if ($gender == "Other") print 'checked' ?>
-                              tabindex="340"> Other </label>
-                <label><input type="radio" 
-                               id="radGenderPrefernottodisclose" 
-                               name="radGender" 
-                               value="Prefer not to disclose"
-                               <?php if ($gender == "Prefer not to disclose") print 'checked' ?>
-                               tabindex="340"> Prefer not to disclose</label>
-            </fieldset>
-
-             
 
         </fieldset> <!-- ends contact -->
        
