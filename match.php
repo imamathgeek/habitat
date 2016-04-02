3bc5c71906be83dbcf543853b46e3b8a43a02d5d
sopalmer <sopalmer@uvm.edu>
2016-04-02 11:52:30 -0400

    grechen's commit

--- match.php
+++ match.php
@@ -10,11 +10,38 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <h1>Match Page</h1>
+        <h1>Meet Your Matches!</h1>
         
+        <?php
+        
+        
+        
+        ?>
+        
+        <?php
+            include("top.php");
+        ?>
+        
+        <p id="des">Welcome to your habitat, a place for you, where you belong. Our team has carefully selected people we feel will complement your living style. Start emailing your matches to have your perfect college roommate experience here at UVM!</p>
         
         <table>
-            <p>hi</p>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Gender</td>
+                    <td>Year</td>
+                    <td>On/Off Campus?</td>
+                    
+                </tr>
+                
+                <tr>
+                    <td>Sally Sunshine</td>
+                    <td>Female</td>
+                    <td>Year 3</td>
+                    <td>Off Campus</td>
+                    
+                </tr>
+            </tbody>
         </table>
     </body>
 </html>
