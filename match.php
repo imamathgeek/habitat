6ab164a179dfdea85db7fcd216dba10364fa40bb
Gretchen <ggirdzis@uvm.edu>
2016-04-02 10:17:13 -0400

    Match

--- match.php
+++ match.php
@@ -9,7 +9,33 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
+    
     <body>
         <h1>Match Page</h1>
+        
+        <table>
+            <tr>
+            <li>Name</li>
+            <li>Gender</li>
+            <li>Year</li>
+            <li>On/Off Campus</li>
+            </tr>
+            
+            <tr>
+            <li>Sally Sunshine</li>
+            <li>Female</li>
+            <li>3</li>
+            <li>Off Campus</li>
+            </tr>
+        </table>
     </body>
+    
+    <?php
+    
+        //Query for matches
+    
+    ?>
+    
+    
+    
 </html>
